153e726f808755dfa69b44667f359c680ae47d25
Philip Howard <phil@gadgetoid.com>
2012-08-22 20:56:51 +0100

    Cleaned up extconf.rb with smarter code

--- ext/wiringpi/extconf.rb
+++ ext/wiringpi/extconf.rb
@@ -1,17 +1,9 @@
 require 'mkmf'
+
+$srcs = Dir.glob('WiringPi/wiringPi/*.c')
+$objs = $srcs.map{ |file| file.sub('.c','.o') }
+$srcs << 'wiringpi_wrap.c'
+$objs << 'wiringpi_wrap.o'
+
 create_makefile('wiringpi/wiringpi')
-
-srcs = Dir.glob('WiringPi/wiringPi/*.c')
-objs = Dir.glob('WiringPi/wiringPi/*.c').map { |file| file.sub('.c','.o') }
-
-srcs = srcs.join(' ')
-objs = objs.join(' ')
-
-makefile = File.open('Makefile', 'r').read
-
-makefile.sub!('SRCS = ','SRCS = ' + srcs + ' ')
-makefile.sub!('OBJS = ','OBJS = ' + objs + ' ')
-
-File.open('Makefile','w') {|file| file.write(makefile)}
-
     
